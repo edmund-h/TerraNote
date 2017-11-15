34f276b39c818c368286187cb52b6da4afa89366
edmund-h <447ght@gmail.com>
2017-11-15 13:07:43 -0500

    architect signincontroller into settingscontroller

--- TerraNote/TNSignInController.swift
+++ TerraNote/TNSignInController.swift
@@ -13,6 +13,13 @@ import GoogleSignIn
 class TNSignInController: UIViewController, GIDSignInUIDelegate {
     
     @IBOutlet weak var googleButton: GIDSignInButton!
+    @IBOutlet weak var settingsContainerView: UIView!
+    @IBOutlet weak var settingsContainerHeight: NSLayoutConstraint!
+    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var blockList: [TNUser] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +28,19 @@ class TNSignInController: UIViewController, GIDSignInUIDelegate {
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().shouldFetchBasicProfile  = true
         
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
-            self.dismiss(animated: false, completion: nil)
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() == false {
+            settingsContainerHeight.constant = 90
+            settingsContainerView.isHidden = false
+            settingsContainerView.subviews.forEach({$0.isUserInteractionEnabled = false})
+        }
+        if let blockData = UserDefaults.standard.dictionary(forKey: TNUser.Property.blocklist.rawValue) as? [String:String] {
+            blockData.forEach({key, value in
+                blockList.append(TNUser(email: value, id: key, channels: [], blocklist: [], notes: []))
+            })
         }
     }
 }
@@ -66,4 +81,32 @@ extension TNSignInController: GIDSignInDelegate {
         //TODO: nuke userdefaults
     }
     
+}
+
+extension TNSignInController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return blockList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = blockList[indexPath.row].email
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let confirmUnblockAlert = UIAlertController(title: "Unblock User", message: "Would you like to unblock this user?", preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "Yes", style: .destructive, handler: {_ in
+            FirebaseClient.unblock(self.blockList[indexPath.row])
+            tableView.reloadData()
+        })
+        let noAction = UIAlertAction(title: "No", style: .cancel, handler: {_ in confirmUnblockAlert.dismiss(animated: true, completion: nil)})
+        confirmUnblockAlert.addAction(yesAction)
+        confirmUnblockAlert.addAction(noAction)
+        self.present(confirmUnblockAlert, animated: true, completion: nil)
+    }
 }
